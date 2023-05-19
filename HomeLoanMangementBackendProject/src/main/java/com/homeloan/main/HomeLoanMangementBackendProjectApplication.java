750e7d20685bf0d13e627bf5d6229ae6dbb90815
Dhanashri3007 <dhanu412admane@gmail.com>
2023-05-19 14:23:08 +0530

    add backend project

---
+++ HomeLoanMangementBackendProject/src/main/java/com/homeloan/main/HomeLoanMangementBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.homeloan.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoanMangementBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoanMangementBackendProjectApplication.class, args);
+	}
+
+}
